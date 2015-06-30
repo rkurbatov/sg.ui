ba76edcce62f8d6c345897cc355e9b4d0ba06aa1
Roman Kurbatov <rkurbatov@gmail.com>
2015-06-30 14:03:49 +0300

    [+] sgMultiColor directive

--- build/sg.ui.js
+++ build/sg.ui.js
@@ -72,6 +72,33 @@
     }
 })();
 
+(function(){
+    'use strict';
+
+    angular
+        .module('sg.ui')
+        .directive('sgMulticolor', sgMulticolor);
+
+    sgMulticolor.$inject = [];
+
+    function sgMulticolor(){
+        var ddo = {
+            restrict: 'C',
+            link: link
+        };
+
+        return ddo;
+
+        function link(scope, elm, attrs) {
+            elm.animate({color: '#909090'}, 500);
+            setInterval(function () {
+                var colors = ['#ff0000', '#ffa500', '#8b4513', '#008000', '#1e80a0', '#8b008b'];
+                var currentColor = colors[Math.floor(Math.random() * colors.length)];
+                $('.sg-multicolor').animate({color: currentColor}, 1000);
+            }, 1000);
+        }
+    }
+})();
 (function () {
     'use strict';
 
