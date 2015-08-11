d18fecfe4a350df5e141a7fe921f259b6ea5704f
Roman Kurbatov <rkurbatov@gmail.com>
2015-08-12 00:31:01 +0300

    repair sgMulticolor

--- src/sgMulticolor.directive.js
+++ src/sgMulticolor.directive.js
@@ -5,9 +5,9 @@
         .module('sg.ui')
         .directive('sgMulticolor', sgMulticolor);
 
-    sgMulticolor.$inject = ['$timeout'];
+    sgMulticolor.$inject = [];
 
-    function sgMulticolor($timeout){
+    function sgMulticolor(){
         var ddo = {
             restrict: 'C',
             link: link
@@ -17,7 +17,7 @@
 
         function link(scope, elm, attrs) {
             elm.animate({color: '#909090'}, 500);
-            $timeout(function () {
+            setTimeout(function () {
                 var colors = ['#ff0000', '#ffa500', '#8b4513', '#008000', '#1e80a0', '#8b008b'];
                 var currentColor = colors[Math.floor(Math.random() * colors.length)];
                 elm.animate({color: currentColor}, 1000);
