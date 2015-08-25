4ab8e96b7ab60c8386e265e3d08196c622b16602
Roman Kurbatov <rkurbatov@gmail.com>
2015-08-25 13:50:15 +0300

    sgElastic directive

--- build/sg.ui.js
+++ build/sg.ui.js
@@ -10,6 +10,34 @@
 
     angular
         .module('sg.ui')
+        .directive('sgElastic', sgElastic);
+
+    sgElastic.$inject = ['$timeout'];
+    function sgElastic($timeout) {
+        return {
+            restrict: 'A',
+            link: link
+        };
+
+        function link($scope, element) {
+            $scope.initialHeight = $scope.initialHeight || element[0].style.height;
+            var resize = function () {
+                element[0].style.height = $scope.initialHeight;
+                element[0].style.height = "" + element[0].scrollHeight + "px";
+            };
+            element.on("input change", resize);
+            $timeout(resize, 0);
+        }
+    }
+
+})(window, window.angular);
+
+
+(function (window, angular, undefined) {
+    'use strict';
+
+    angular
+        .module('sg.ui')
         .directive('sgLowerCase', sgLowerCase);
 
     sgLowerCase.$inject = ['$parse'];
