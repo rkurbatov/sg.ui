c6ca2977611f7d10d22fd3bd9bd87f537727f3b4
Roman Kurbatov <rkurbatov@gmail.com>
2015-09-13 02:05:35 +0300

    new push

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 'use strict';
 
 var gulp = require('gulp');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-var DEST = 'build/';
+var DEST = 'dist/';
+var version = require('./package.json').version;
 
-gulp.task('default', createDeploymentVersion);
+gulp.task('default', build);
+gulp.task('bumpMajor', bumpVersion('major'));
+gulp.task('bumpMinor', bumpVersion('minor'));
+gulp.task('bumpPatch', bumpVersion('patch'));
+gulp.task('pushRelease', ['tagRelease'], pushRelease);
+gulp.task('build', build);
+gulp.task('commit', ['build'], commit);
+gulp.task('tagRelease', ['commit'], tagRelease);
 
-function createDeploymentVersion() {
+
+function build() {
     return gulp.src(['src/sg.ui.js', 'src/*.js'])
         .pipe(concat('sg.ui.js'))
         // This will output the non-minified version
@@ -18,4 +29,36 @@ function createDeploymentVersion() {
         .pipe(uglify())
         .pipe(rename({extname: '.min.js'}))
         .pipe(gulp.dest(DEST));
+}
+
+function commit(){
+    return gulp.src('./build/**/*')
+        .pipe(git.commit('Dist files for version ' + version))
+        .pipe(git.pull())
+}
+
+function tagRelease() {
+    git.tag('v'+ version, 'Tagging version '+ version, function(err){
+        if (err) throw err;
+    });
+}
+
+function pushRelease() {
+    git.push('origin', 'master', function(err){
+        if(err) throw err;
+    });
+    git.push('origin', 'master', {args: '--tags'}, function(err){
+        if (err) throw err;
+    });
+}
+
+function bumpVersion(bumpType) {
+    return function () {
+        var version = null;
+
+        gulp.src('./package.json')
+            .pipe(bump({type: bumpType}))
+            .pipe(gulp.dest('./'))
+            .pipe(git.commit("Bump package version: " + version));
+    }
 }
