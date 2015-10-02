3944e32e53b1327cce869c217fdd37f075eeff2f
Roman Kurbatov <rkurbatov@gmail.com>
2015-10-03 01:11:31 +0300

    sg-on-img-error directive

---
+++ src/sgOnImgError.directive.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    // directive runs function on image load
+    angular
+        .module('sg.ui')
+        .directive('sgOnImgError', sgOnImgError);
+
+    sgOnImgError.$inject = ['$parse'];
+
+    function sgOnImgError($parse) {
+        return {
+            restrict: 'A',
+            link: link
+        };
+
+        function link(scope, elm, attrs) {
+            var fn = $parse(attrs.sgOnImgError);
+            elm.on('error', applyFunction);
+
+            scope.$on('$destroy', function(){
+                elm.off('error', applyFunction);
+            });
+
+            function applyFunction(event) {
+                scope.$applyAsync(function () {
+                    fn(scope, {$event: event});
+                });
+            }
+        }
+    }
+
+})();
+
